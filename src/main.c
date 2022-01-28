451cc516724be89f5b69a5b6c9dd4e7003159fd5
Frederik SCHLUTER <fschlute@e2r11p8.clusters.42paris.fr>
2022-01-28 16:32:05 +0100

    first cleaning part

--- src/main.c
+++ src/main.c
@@ -6,14 +6,8 @@
 # include <mlx.h>
 #endif
 
-void	dummy(t_data *img, char *file_name)
+void	dummy(t_data *img)
 {
-	img->map = parse_map(file_name);
-	if (! img->map)
-	{
-		ft_putendl_fd("img->map emptey sa mer", 1);
-		return ;
-	}
 	zoom_grid(img->map, 1, 1);
 	project_grid(img->map);
 	t_dimension_2d current_dimensions = measure_necessary_screen_space(img->map);
@@ -42,29 +36,37 @@ int free_mlx(int keycode, t_data *img)
 	return 0;
 }
 
+int init_mlx(t_data *img)
+{
+	(*img).window_size.x_max = 2200;
+	(*img).window_size.y_max = 1000;
+	(*img).mlx = mlx_init();
+	(*img).mlx_win = mlx_new_window((*img).mlx, (*img).window_size.x_max, (*img).window_size.y_max, "Fild D'enFer");
+	(*img).img = mlx_new_image((*img).mlx, (*img).window_size.x_max, (*img).window_size.y_max);
+	(*img).addr = mlx_get_data_addr((*img).img, &(*img).bits_per_pixel, &(*img).line_length, &(*img).endian);
+	return (0);
+}
+
 #ifndef TESTING
+
+
 
 int	main(int argc, char **argv)
 {
 	t_data	img;
 
-	img.window_size.x_max = 2200;
-	img.window_size.y_max = 1000;
-	img.mlx = mlx_init();
-	img.mlx_win = mlx_new_window(img.mlx, img.window_size.x_max, img.window_size.y_max, "Hello world!");
-	img.img = mlx_new_image(img.mlx, img.window_size.x_max, img.window_size.y_max);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	//test_pass_param(&img);
-	if (argc == 2)
-	{
-		dummy(&img, argv[1]);
-		mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
-		mlx_key_hook(img.mlx_win, free_mlx, &img);
-		mlx_loop(img.mlx);
-		puts("stopped");
-	}
+	if (argc != 2)
+		return (1);
+	img.map = parse_map(argv[1]);
+	if (! img.map)
+		return (1);
+	init_mlx(&img);
+	dummy(&img);
+	mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
+	mlx_key_hook(img.mlx_win, free_mlx, &img);
+	mlx_loop(img.mlx);
+	puts("stopped");
 	return (0);
 }
 
 #endif
- //*/
